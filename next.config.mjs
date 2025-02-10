7d03a72620a0ce478bd942368aed3739bab351e1
RevanToma <tamrevan@gmail.com>
2025-02-10 19:22:41 +0100

    hubpages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  basePath: 'https://github.com/RevanToma/quiz',
 };
 
 export default nextConfig;
